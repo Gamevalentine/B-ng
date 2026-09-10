8d593fcedb8e3a47b358de2578aa9c2b6866a732
Gamevalentine <anhtsizoo00@gmail.com>
2026-09-10 11:18:54 +0700

    fix(stage-ui): remove duplicate store return entry

--- packages/stage-ui/src/stores/settings/general.ts
+++ packages/stage-ui/src/stores/settings/general.ts
@@ -32,7 +32,6 @@ export const useSettingsGeneral = defineStore('settings-general', () => {
   return {
     language,
     disableTransitions,
-    disableTransitions,
     usePageSpecificTransitions,
     websocketSecureEnabled,
     getLanguage,
